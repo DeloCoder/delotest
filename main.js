351f2146bebab7f697cd01fef42b3f8eac34fdf3
ivan-codesmith <ivan.viveros@codesmithdev.com>
2022-05-05 14:31:04 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+//var
+var empleado = "juanito perez";
+console.log(empleado);
+
+var empleado = "juanito lopez";
+console.log(empleado);
+//let data
+
+
+// let producto = "azucar";
+// console.log(typeof producto);
+// let producto = "manzana";
+// console.log(producto);
+
+let nombreEmpleado = "DIEGO DEL HOYO"; //string
+let precioProducto = 2000 //number
+let miVariableBooleana = true // boolean(true o false)
+
+console.log(nombreEmpleado);
+console.log(precioProducto);
+console.log(miVariableBooleana);
+
+
+//suma
+let a = 2;
+let b = 3;
+let resultado = a+b;
+console.log("suma: " , resultado);
+
+
+console.log("hola", 1,2,3,4,false);
+
+
+
+
+
+
+
+
+
+//const
+const carrito = "carrito de mercado";
+
+
